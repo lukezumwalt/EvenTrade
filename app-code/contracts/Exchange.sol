9ef7da9c0dfebf47097ce04372c192cc402df965
zumwa020 <zumwa020@umn.edu>
2022-11-10 19:01:27 -0700

    smart contract and deployment blanks

---
+++ app-code/contracts/Exchange.sol
@@ -0,0 +1,22 @@
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Exchange {
+    // Smoke Test
+    //  set up properly
+    //  can't be deployed
+    //  responds as expected
+
+    // Constructor
+    constructor() public {
+        ticketName = "deadAndCompany";
+    }
+    
+    // Store ticket information
+    string public ticketName;
+
+    // Store sale information
+
+    // Read ticket information
+
+    // Read sale information
+}
