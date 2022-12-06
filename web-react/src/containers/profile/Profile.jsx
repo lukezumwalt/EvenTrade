5573aad30a7bcb23440cfa2209ad81cde1586b77
Marie <mehargan@gmail.com>
2022-12-05 21:21:46 -0700

    profile formatting start

--- web-react/src/containers/profile/Profile.jsx
+++ web-react/src/containers/profile/Profile.jsx
@@ -2,18 +2,28 @@
 
 import React from "react";
 import { useAuth0 } from '@auth0/auth0-react';
+import Feature from "../../components/feature/Feature";
+import "./profile.css"
 
 const Profile = () => {
     const { user, isAuthenticated } = useAuth0();
 
     return (
         isAuthenticated && (
-            <article className='column' id="profile">
-                {user.picture && <img src={user.picture} alt={user.name} />}
-                <h2>{user.name}</h2>
-                <ul>
-                    {Object.keys(user).map((objKey, i) => <li key={i}>{objKey}: {user[objKey]} </li>)}
-                </ul>
+            <article className='eventrade__profile' id="profile">
+                <div className="eventrade__profile-heading">
+                    <div className="eventrade__profile-image">
+                        {user.picture && <img src={user.picture} alt={user.name} />}
+                        <div className="eventrade__profile-feature">
+                            <Feature title={user.name}></Feature>
+                        </div>
+                    </div>
+                    <p>
+                        <ul>
+                            {Object.keys(user).map((objKey, i) => <li key={i}>{objKey}: {user[objKey]} </li>)}
+                        </ul>
+                    </p>
+                </div>
             </article>
         )
     )
